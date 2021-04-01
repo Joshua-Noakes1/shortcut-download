d845221548add7539a5f50ddbd4f5a1489e45e6c
Joshua Noakes <joshua.noakes1@icloud.com>
2021-04-01 22:23:34 +0100

    setup server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+// https://github.com/Joshua-Noakes1/shortcut-download
+
+// Node http server
+const http = require('http');
+const app = require('./app.js');
+require('dotenv').config();
+
+// defaults to port 3000 if no port can be found
+const port = process.env.PORT || 3000;
+
+// create server
+const server = http.createServer(app);
+server.listen(port);
+
+console.log(`🚀 Started server on port ${port} 🚀`);
