40f0624c4fa6d0dedcff563a0eff463d56a156b7
Joshua Noakes <joshua.noakes1@icloud.com>
2021-04-03 01:39:48 +0100

    i think it works reddit and twitter

--- endpoint/reddit/reddit.js
+++ endpoint/reddit/reddit.js
@@ -4,21 +4,28 @@ const fetch = require('node-fetch');
 const url = require('url');
 
 router.post('/download', async (req, res, next) => {
-    const reddit = await fetch(`${req.body.url}.json`);
+    // remove the ?utm that reddit adds from the share button
+    const post_url = req.body.url.toString().match(/.+?(?=\?)/);
+
+    // get json from reddit about post
+    const reddit = await fetch(`${post_url}.json`);
     const reddit_json = await reddit.json();
 
-    const split_url_regex = '/(http[s]?:\/\/)?([^\/\s]+\/)(.*)/';
-
+    // find the fallback video url in the json
     const fallback_url = `${reddit_json[0].data.children[0].data.secure_media.reddit_video.fallback_url}`;
 
+    // do some regex to get just the id from the fallback url | https://v.redd.it/0sft9w7j8sq61/DASH_720.mp4?source=fallback > 0sft9w7j8sq61
     let parsed_url
     parsed_url = url.parse(fallback_url);
     parsed_url = parsed_url.pathname.toString().match(/.+?(?=D)/)[0];
     parsed_url = parsed_url.toString().replace(/\//g, '');
-    console.log(parsed_url);
+
+    // use vred.rip api to get combined audio and video 
+    const vred = await fetch(`https://vred.rip/api/vreddit/${parsed_url}`);
+    const vred_json = await vred.json();
 
     res.status(200).json({
-        url: `${reddit_json[0].data.children[0].data.secure_media.reddit_video.fallback_url}`
+        url: `${vred_json.source}`
     });
 });
 
